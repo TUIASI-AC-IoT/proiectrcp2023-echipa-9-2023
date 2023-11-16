72f61fe3e95a76e351e386eac1ed72b642b69ba3
vladapostol1 <vladapostol1029@gmail.com>
2023-11-17 00:27:38 +0200

    Subscribe/Publish

--- client.py
+++ client.py
@@ -1,18 +1,32 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
-PORT = 1883        
+PORT = 1883
+
+def receive_messages(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            print(f"Received message: {message}")
+        except ConnectionResetError:
+            print("Connection closed by server.")
+            break
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((HOST, PORT))
+
+    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    receive_thread.start()
+    print("Enter 'subscribe topic' or 'topic message' (or 'exit' to quit): ")
     while True:
-        message = input("Enter a message to send (or 'exit' to quit): ")
-        if message.lower() == 'exit':
+        user_input = input()
+        if user_input.lower() == 'exit':
             break
-        client.send(message.encode('utf-8'))
-        response = client.recv(1024)
-        print(f"Received from server: {response.decode('utf-8')}")
+        client.send(user_input.encode('utf-8'))
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -4,30 +4,79 @@ import threading
 HOST = '127.0.0.1'
 PORT = 1883
 
-# Function to handle each client
-def handle_client(client_socket):
-    while True:
-        message = client_socket.recv(1024).decode('utf-8')
-        if not message:
-            break
-        decoded_message = decode_message(message)
-        print(f"Received from client: {decoded_message}")
+# Dictionary to store topics, subscribers, and last messages
+topics = {}
 
-        ack_message = encode_message("ACK: " + decoded_message)
-        client_socket.send(ack_message.encode('utf-8'))
+# Acknowledgment codes and messages
+ACK_SUBSCRIBE = "SUBACK"
+ACK_PUBLISH = "PUBACK"
+ERROR_INVALID_COMMAND = "ERR_INVALID_COMMAND"
+
+# Function to handle each client
+def handle_client(client_socket, client_address):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                break
+            if message.startswith("subscribe "):
+                subscribe_topic(client_socket, message)
+            else:
+                publish_message(client_socket, message)
+        except ConnectionResetError:
+            print(f"Client at {client_address[0]}:{client_address[1]} forcibly closed the connection.")
+            break
     client_socket.close()
 
-def encode_message(message):
-    """
-    Return the message as is.
-    """
-    return message
+def subscribe_topic(client_socket, message):
+    topic = message.split(" ")[1]
+    if topic not in topics:
+        topics[topic] = {'subscribers': [], 'last_message': ''}
+        # Print the updated topic list when a new topic is created
+        print(f"New topic created: {topic}")
+        print("Current topics:", list(topics.keys()))
+    topics[topic]['subscribers'].append(client_socket)
 
-def decode_message(message):
-    """
-    Return the message as is.
-    """
-    return message
+    # Print all subscriber sockets for this topic
+    print(f"Subscribers for topic '{topic}':")
+    for subscriber in topics[topic]['subscribers']:
+        print(subscriber.getpeername())
+
+    # Send SUBACK acknowledgment
+    client_socket.send(ACK_SUBSCRIBE.encode('utf-8'))
+
+def publish_message(client_socket, message):
+    parts = message.split(" ", 1)
+    if len(parts) != 2:
+        # Invalid command format
+        client_socket.send(ERROR_INVALID_COMMAND.encode('utf-8'))
+        return
+
+    topic, msg = parts
+    if topic in topics:
+        topics[topic]['last_message'] = msg
+        for subscriber_socket in topics[topic]['subscribers']:
+            try:
+                subscriber_socket.send(msg.encode('utf-8'))
+            except:
+                # Handle connection issues
+                subscriber_socket.close()
+                topics[topic]['subscribers'].remove(subscriber_socket)
+
+        # Send the message back to the sender if they are subscribed
+        if client_socket in topics[topic]['subscribers']:
+            try:
+                client_socket.send(msg.encode('utf-8'))
+            except:
+                # Handle connection issues
+                client_socket.close()
+                topics[topic]['subscribers'].remove(client_socket)
+
+        # Send PUBACK acknowledgment
+        client_socket.send(ACK_PUBLISH.encode('utf-8'))
+    else:
+        # Send error message if the topic doesn't exist
+        client_socket.send(f"ERROR: Topic '{topic}' does not exist.".encode('utf-8'))
 
 def main():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -36,11 +85,17 @@ def main():
     print(f"[*] Server was started on {HOST}:{PORT}")
 
     while True:
-        client_sock, address = server.accept()
-        print(f"[*] Accepted connection from {address[0]}:{address[1]}")
-        
-        client_handler = threading.Thread(target=handle_client, args=(client_sock,))
-        client_handler.start()
+        try:
+            client_sock, address = server.accept()
+            print(f"[*] Accepted connection from {address[0]}:{address[1]}")
+
+            # Pass client address as an argument to handle_client
+            client_handler = threading.Thread(target=handle_client, args=(client_sock, address))
+            client_handler.start()
+        except KeyboardInterrupt:
+            print("\nServer is shutting down...")
+            server.close()
+            break
 
 if __name__ == "__main__":
     main()
